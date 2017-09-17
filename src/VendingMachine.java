a4fa62336ac6df7a2e32eab54e7f1b50d50da205
chasestruse <chasestruse@gmail.com>
2017-09-16 20:36:22 -0500

    MakeChange() is passed a number and it returns it

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -8,21 +8,24 @@ public class VendingMachine {
     float totalChangeGivenBack;
     float totalOfPurchase;
 
-    public float makeChange(String inputString) {
+
+    public float makeChange(String inputString, float total) {
+
+        totalOfPurchase = total;
 
         if(inputString.equalsIgnoreCase("Dollar")){
-            totalMoneyPutIntoMachine = 1.0f;
+            totalMoneyPutIntoMachine += 1.0f;
         }else if(inputString.equalsIgnoreCase("Quarter")){
-            totalMoneyPutIntoMachine = 0.25f;
+            totalMoneyPutIntoMachine += 0.25f;
         }else if(inputString.equalsIgnoreCase("Dime")){
-            totalMoneyPutIntoMachine = 0.10f;
+            totalMoneyPutIntoMachine += 0.10f;
         }else if(inputString.equalsIgnoreCase("Nickel")){
-            totalMoneyPutIntoMachine = 0.05f;
+            totalMoneyPutIntoMachine += 0.05f;
         }else if(inputString.equalsIgnoreCase("Penny")){
-            totalMoneyPutIntoMachine = 0.00f;
+            totalMoneyPutIntoMachine += 0.00f;
         }else{
-            totalMoneyPutIntoMachine = 0.0f;
+            totalMoneyPutIntoMachine += 0.0f;
         }
-        return totalMoneyPutIntoMachine;
+        return totalOfPurchase - totalMoneyPutIntoMachine;
     }
 }
--- test/VendingMachineTest.java
+++ test/VendingMachineTest.java
@@ -20,7 +20,13 @@ public class VendingMachineTest {
     @Test
     public void whenMakeChangeIsPassedANameOfACoinItAddsThatCoinsValueToAFloat(){
         String quarter = "Quarter";
-        assertEquals(0.25f, vendingMachine.makeChange(quarter));
+        assertEquals(0.25f, vendingMachine.makeChange(quarter, 0.50f));
+    }
+    @Test
+    public void whenMakeChangeIsPassedATotalItReturnsTheTotalMinusTheChangePutIn(){
+        String quarter = "Quarter";
+        float total = 1.25f;
+        assertEquals(1.0f, vendingMachine.makeChange(quarter, total));
     }
 
 }
